47d492e1aabf606f6ae33fa697f94222e5b5a1d1
nhieunguyen1510 <vannhieunguyen1994@gmail.com>
2017-03-23 10:53:16 +0700

    Format code, Add build command

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
-concat = require('gulp-concat'),
-minify = require('gulp-minify'),
-uglify = require('gulp-uglify');
+    concat = require('gulp-concat'),
+    minify = require('gulp-minify'),
+    uglify = require('gulp-uglify');
 gulp.task('js', function() {
     return gulp.src(['resources/assets/js/**/*.js'])
         .pipe(concat('main.js'))
