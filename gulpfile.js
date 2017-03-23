e2fa2133a41a9fec25c287693a0ef89acabe65cd
Nhieu Nguyen Van <nhieunguyenv@HarveyNash.vn.local>
2017-03-23 10:20:16 +0700

    Config gulpfle to build (minify and uglify) javascript files

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+concat = require('gulp-concat'),
+minify = require('gulp-minify'),
+uglify = require('gulp-uglify');
+gulp.task('js', function() {
+    return gulp.src(['resources/assets/js/**/*.js'])
+        .pipe(concat('main.js'))
+        .pipe(minify())
+        .pipe(uglify())
+        .pipe(gulp.dest('public/js/'));
+});
+gulp.task('default', ['js'], function(){});
