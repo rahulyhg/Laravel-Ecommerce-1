efaf494e415b79c7f20ce62182378cd32792d8e6
nhieunguyen1510 <vannhieunguyen1994@gmail.com>
2017-04-07 16:38:49 +0700

    Customize reset password token

--- Laravel/app/Http/Controllers/Auth/ResetPasswordController.php
+++ Laravel/app/Http/Controllers/Auth/ResetPasswordController.php
@@ -47,8 +47,6 @@ class ResetPasswordController extends Controller
      */
     public function reset(ResetPasswordPost $request)
     {
-        $this->validate($request, $this->rules(), $this->validationErrorMessages());
-
         // Here we will attempt to reset the user's password. If it is successful we
         // will update the password on an actual user model and persist it to the
         // database. Otherwise we will parse the error and return the response.
